2c2bc5c969ea04fbb4a84fc1340ed675c4fce466
t-tajiri <swallow.ava@gmail.com>
2020-03-16 15:53:05 +0900

    feat: add logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { AuthContext } from '../App'
 
 export function Header () {
+  const { state, dispatch } = React.useContext(AuthContext)
   return (
     <nav id="navigation">
-      <a href="#top">
-        <h1 className="logo">
-          hooks-music
-        </h1>
-      </a>
+      <h1 className="logo">
+        hooks-music
+      </h1>
+      <button onClick={() => dispatch({ type: 'LOGOUT' })}>
+        {state.isAuthenticated && (
+          <h1>Hi {state.user.firstName} (LOGOUT)</h1>
+        )}
+      </button>
     </nav>
   )
 }
